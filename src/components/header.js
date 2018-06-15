a9e3062e84c8c945e0c8f04b51bec4dbdf40c367
Lucas Van Skyhawk <luke@vanskyhawk.com>
2018-06-15 13:43:19 -0600

    Added custom header skews and border

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Header = (title) => {
     return (
         <div className="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>{title}</h1>
             <p>ayy yo</p>
-        </div>
+        </div >
     )
 }
 
